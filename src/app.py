67f44e8ae495d564628e9d562dadaf4081aea494
Pekia23 <pekia76@gmail.com>
2024-09-06 16:18:46 -0500

    arreglitos

--- src/app.py
+++ src/app.py
@@ -1,7 +1,6 @@
 
 from flask import Flask, render_template, request,jsonify, redirect, url_for
 from database import buscar_equipos 
-
 from config import config
 from flask import Flask, jsonify
 from database import verificar_conexion
