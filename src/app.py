537a3cbeb9269115869d089667746052e033f2c7
Pekia23 <pekia76@gmail.com>
2024-09-06 15:01:36 -0500

    reorganizar

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 import MySQLdb
-from flask import Flask, render_template, request,jsonify
+from flask import Flask, redirect, render_template, request,jsonify, url_for
 from flask_mysqldb import MySQL
 from config import config
 from flask import Flask, jsonify
@@ -20,7 +20,7 @@ def check_db_connection():
     except MySQLdb.Error as e:
         return jsonify({"status": "error", "message": str(e)}), 500
 
-@app.route('/')
+@app.route('/', endpoint ='inicio')
 def home():
     return render_template('index.html')
 
@@ -31,92 +31,83 @@ def lsa():
     query = "SELECT nombre_equipo FROM equipo_info WHERE nombre_equipo LIKE '{}%' ORDER BY nombre_equipo".format(searchbox)
     cursor.execute(query)
     result = cursor.fetchall()
-    return jsonify(result)
+    return redirect(url_for('inicio'))
 
-@app.route('/LSA/equipo-nuevo', methods=['POST','GET'])
-def equipo_nuevo():
-    return render_template('equipo_nuevo.html')
-
-@app.route('/LSA/equipo')
+@app.route('/LSA/mostrar-equipo')
 def mostrar_equipo():
     return render_template('mostrar_equipo.html')
 
-@app.route('/LSA/equipo/FMEA')
-def fmea():
+@app.route('/LSA/equipo/mostrar-FMEA')
+def mostrar_FMEA():
     return render_template('mostrar_FMEA.html')
 
-@app.route('/LSA/equipo/MTA')
-def mta():
+@app.route('/LSA/equipo/mostrar-MTA')
+def mostrar_MTA():
     return render_template('mostrar_MTA.html')
 
-@app.route('/LSA/equipo/RCM')
-def rcm():
+@app.route('/LSA/equipo/mostrar-RCM')
+def mostrar_RCM():
     return render_template('mostrar_RCM.html')
 
-@app.route('/LSA/equipo/LORA')
-def lora():
+@app.route('/LSA/equipo/mostrar-LORA')
+def mostrar_lora():
     return render_template('mostrar_lora.html')
 
-@app.route('/LSA/equipo/analisis-funcional')
-def analisis_funcional():
+@app.route('/LSA/equipo/mostrar-analisis-funcional')
+def mostrar_analisis_funcional():
     return render_template('mostrar_analisis-funcional.html')
 
-@app.route('/LSA/equipo/herramientas-especiales')
-def herramientas_especiales():
+@app.route('/LSA/equipo/mostrar-herramientas-especiales')
+def mostrar_herramientas_especiales():
     return render_template('mostrar_herramientas-especiales.html')
 
-@app.route('/LSA/equipo/analisis-herramientas')
-def analisis_herramientas():
+@app.route('/LSA/equipo/mostrar-analisis-herramientas')
+def mostrar_analisis_herramientass():
     return render_template('mostrar_analisis-herramientas.html')
 
-@app.route('/LSA/equipo/repuestos')
-def repuestos():
+@app.route('/LSA/equipo/mostrar-repuestos')
+def mostrar_repuesto():
     return render_template('mostrar_repuesto.html')
 
-@app.route('/LSA/prueba')
-def prueba():
-    return render_template('prueba.html')
+@app.route('/LSA/registro-MTA-2')
+def registro_MTA_2():
+    return render_template('registro_MTA_2.html')
 
+@app.route('/LSA/registro-MTA')
+def registro_MTA():
+    return render_template('registro_MTA.html')
 
-@app.route('/LSA/registro-mta-2')
-def registro_mta_2():
-    return render_template('registro_mta_2.html')
+@app.route('/LSA/registro-RCM-2')
+def registro_RCM_2():
+    return render_template('registro_RCM_2.html')
 
-@app.route('/LSA/registro-mta')
-def registro_mta():
-    return render_template('registro_mta.html')
+@app.route('/LSA/registro-RCM')
+def registro_RCM():
+    return render_template('registro_RCM.html')
 
-@app.route('/LSA/registro-rcm-2')
-def registro_rcm_2():
-    return render_template('registro_rcm_2.html')
+@app.route('/LSA/registro-generalidades')
+def registro_generalidades():
+    return render_template('registro_generalidades.html')
 
-@app.route('/LSA/registro-rcm')
-def registro_rcm():
-    return render_template('registro_rcm.html')
+@app.route('/LSA/registro-FMEA')
+def registro_FMEA():
+    return render_template('registro_FMEA.html')
 
-@app.route('/LSA/generalidades')
-def generalidades():
-    return render_template('generalidades.html')
+@app.route('/LSA/registro-FMEA-2')
+def registro_FMEA_2():
+    return render_template('registro-FMEA_2.html')
 
-@app.route('/LSA/ingresar-FMEA')
-def ingresarFMEA():
-    return render_template('ingresar_FMEA.html')
+@app.route('/LSA/registro-analisis-funcional')
+def registro_analisis_funcional():
+    return render_template('registro_analisis_funcional.html')
 
-@app.route('/LSA/fmea-form-2')
-def fmea_form_2():
-    return render_template('fmea-form-2.html')
+@app.route('/LSA/registro-herramientas-especiales')
+def registro_herramientas_especiales():
+    return render_template('registro_herramientas_especiales.html')
 
-@app.route('/LSA/analisis-funcional')
-def analisis_funcional():
-    return render_template('analisis_funcional.html')
-
-@app.route('/LSA/herramientas-especiales')
-def herramientas_especiales():
-    return render_template('herramientas_especiales.html')
-
-@app.route('/LSA/repuesto')
-def repuesto():
-    return render_template('repuesto.html')
+@app.route('/LSA/registro-repuesto')
+def registro_repuesto():
+    return render_template('registro_repuesto.html')
 
 
 
